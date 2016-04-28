1eb11537a3296bc3fbb5af54847e164d905a9dad
Song Wei <1983.song.wei@gmail.com>
2016-04-28 20:21:28 +0000

    t

---
+++ server.js
@@ -0,0 +1,104 @@
+// // reference the http module so we can create a webserver
+// var http = require("http");
+
+// // create a server
+// http.createServer(function(req, res) {
+//     // on every request, we'll output 'Hello world'
+//     res.end("Hello world from Cloud9!");
+// }).listen(process.env.PORT, process.env.IP);
+
+// // Note: when spawning a server on Cloud9 IDE, 
+// // listen on the process.env.PORT and process.env.IP environment variables
+
+// // Click the 'Run' button at the top to start your server,
+// // then click the URL that is emitted to the Output tab of the console
+const VIDEO_SCALE = 5
+const CATEGORY_SCALE = 5
+
+var _ = require('lodash');
+
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({
+    port: process.env.PORT,
+    host: process.env.IP
+});
+var Wreck = require('wreck');
+const url = "https://storage.googleapis.com/android-tv/android_tv_videos_new.json";
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply) {
+        Wreck.get(url, function(err, res, payload) {
+            const headers = ['cache-control', 'content-type'];
+
+            if (err) {
+                console.log(err);
+                reply.continue;
+                return
+            }
+            var res_str = payload.toString();
+            var obj = JSON.parse(res_str);
+            if (obj) {
+                obj = manipulate_google_video_feed(obj);
+                res_str = JSON.stringify(obj);
+            }
+            var response = reply(res_str).hold();
+            for (var i in headers) {
+                var h = headers[i]
+                if (res.headers[h]) {
+                    response.header(h, res.headers[h]);
+                }
+            }
+            // console.log(response.headers);
+            response.send();
+        });
+    }
+});
+
+server.start(function() {
+    console.log('Server running at:', server.info.uri);
+});
+
+
+function more_videos(obj) {
+    obj.googlevideos = _.map(obj.googlevideos, function(list) {
+        // console.log(list.category)
+        var rrr = _.flatMap(list.videos, function(video) {
+            // console.log(video.title)
+            var video_string = JSON.stringify(video);
+            var rr = _.map(_.range(VIDEO_SCALE), function(index) {
+                    var r = JSON.parse(video_string);
+                    r.title += index
+                    return r
+                })
+                //   console.log(rr);
+            return rr;
+        });
+        //   console.log(rrr);
+        list.videos = rrr;
+        return list;
+    });
+    return obj;
+}
+
+function more_categories(obj) {
+    obj.googlevideos = _.flatMap(obj.googlevideos, function(list) {
+        // console.log(list.category)
+        var list_string = JSON.stringify(list);
+        var rr = _.map(_.range(CATEGORY_SCALE), function(index) {
+                var r = JSON.parse(list_string);
+                r.category += index
+                return r
+            })
+            //   console.log(rr);
+        return rr;
+    });
+    return obj;
+}
+
+function manipulate_google_video_feed(obj) {
+    console.log(obj)
+    return more_videos(obj);
+}
